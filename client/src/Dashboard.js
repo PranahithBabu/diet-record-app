2ff568b601a15d49ead182966c8d87c0acfe7af0
PranahithBabu <59257365+PranahithBabu@users.noreply.github.com>
2023-07-30 18:43:30 -0400

    Update Dashboard.js

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link, Navigate, useLocation } from 'react-router-dom'
 import axios from 'axios'
-import Navbar from './Navbar';
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
 
