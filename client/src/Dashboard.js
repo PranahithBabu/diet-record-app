7de4565dc5b652257eee6ffa9bab9d597bcd04bf
PranahithBabu <59257365+PranahithBabu@users.noreply.github.com>
2023-08-04 18:05:06 -0400

    Update Dashboard.js

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -110,7 +110,8 @@ const Dashboard = () => {
     const year = inputDate.getUTCFullYear();
     const month = String(inputDate.getUTCMonth() + 1).padStart(2, "0");
     const day = String(inputDate.getUTCDate()).padStart(2, "0");
-    const hours = String(inputDate.getUTCHours()).padStart(2, "0");
+    // const hours = String(inputDate.getUTCHours()).padStart(2, "0");
+    const hours = "00"
     const minutes = String(inputDate.getUTCMinutes()).padStart(2, "0");
     const seconds = String(inputDate.getUTCSeconds()).padStart(2, "0");
     console.log("Hours: ",hours)
